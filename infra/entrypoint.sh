e716ff55bb7a844f8a2b1c2d404518778d1e7946
Siddhartha Rauniyar <siddhartharauniyar5@gmail.com>
2026-09-08 12:01:57 +0545

    Fix Docker local development configuration

--- infra/entrypoint.sh
+++ infra/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+
 set -eu
 
+export DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-config.settings.development}"
+
 python manage.py migrate --noinput
+
 python manage.py collectstatic --noinput
+
 exec "$@"
